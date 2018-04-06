98d13de5b346dfd514ae1d5386b94b8a1deed725
NikolaA22 <32622417+NikolaA22@users.noreply.github.com>
2018-04-06 19:43:58 +0200

    first commit

---
+++ driver.py
@@ -0,0 +1,23 @@
+# Your job in this assignment is to write driver.py, which solves any 8-puzzle board when given an
+# arbitrary starting configuration. The program will be executed as follows:
+#
+# $ python driver.py <method> <board>
+#
+# The method argument will be one of the following. You need to implement all three of them:
+#
+# bfs (Breadth-First Search)
+#
+# dfs (Depth-First Search)
+#
+# ast (A-Star Search)
+#
+# The board argument will be a comma-separated list of integers containing no spaces.
+# For example, to use the bread-first search strategy to solve the input board
+# given by the starting configuration {0,8,7,6,5,4,3,2,1},
+# the program will be executed like so (with no spaces between commas):
+#
+# $ python driver.py bfs 0,8,7,6,5,4,3,2,1
+#
+
+
+
